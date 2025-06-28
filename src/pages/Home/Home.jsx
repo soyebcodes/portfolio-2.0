a8d0c6b5463bee2ea248398d91d30f1c1ceb8cd0
soyebcodes <soyeb10191@gmail.com>
2025-06-28 20:47:44 +0600

    Implement skills section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../HeroSection/HeroSection";
 import AboutMe from "../AboutMe/AboutMe";
+import SkillsSection from "../SkillsSection/SkillsSection";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <AboutMe />
+      <SkillsSection />
     </div>
   );
 };
---
+++ src/pages/SkillsSection/SkillsSection.jsx
@@ -0,0 +1,52 @@
+import { motion } from "motion/react";
+
+const skills = [
+  "JavaScript",
+  "React",
+  "Nextjs",
+  "MongoDB",
+  "Express",
+  "Firebase",
+  "HTML5",
+  "CSS3",
+  "Tailwind",
+  "Bootstrap",
+  "Git",
+  "Node.js",
+  "Postman",
+  "Figma",
+];
+
+const SkillsTechnologies = () => {
+  return (
+    <motion.section
+      id="skills"
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+      viewport={{ once: true }}
+      className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-16"
+    >
+      <h2
+        className="font-dillan text-3xl mb-8 text-[#008236]"
+        style={{ WebkitTextStroke: "1px black" }}
+      >
+        Skills & Technologies
+      </h2>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {skills.map((skill, index) => (
+          <motion.div
+            key={index}
+            whileHover={{ scale: 1.05 }}
+            className="bg-white shadow-md rounded-lg px-4 py-3 text-center  font-medium text-gray-800 border border-[#008236] hover:border-[#008236] hover:text-[#008236] transition"
+          >
+            {skill}
+          </motion.div>
+        ))}
+      </div>
+    </motion.section>
+  );
+};
+
+export default SkillsTechnologies;
