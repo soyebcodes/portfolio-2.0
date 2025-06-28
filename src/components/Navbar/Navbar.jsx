a6f6f524a5bdd3db80f9e363902f10baa6b37248
soyebcodes <soyeb10191@gmail.com>
2025-06-28 17:59:09 +0600

    Initial setup of the project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import { NavLink } from "react-router";
+import { Star } from "lucide-react";
+
+const Navbar = () => {
+  const navLinks = [
+    { name: "Home", to: "/" },
+    { name: "About", to: "/about" },
+    { name: "Projects", to: "/projects" },
+    { name: "Contact", to: "/contact" },
+  ];
+
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-white shadow z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap  items-center justify-center md:justify-between h-16 gap-4">
+          {/* Logo */}
+          <NavLink
+            to="/"
+            className="font-space-text flex gap-2 items-center text-2xl font-bold text-[#008236]"
+          >
+            <Star fill="green" />
+            Soyeb Codes
+          </NavLink>
+
+          {/* Nav Links */}
+          <div className="flex flex-wrap gap-4">
+            {navLinks.map((link) => (
+              <NavLink
+                key={link.name}
+                to={link.to}
+                className={({ isActive }) =>
+                  `font-inter transition px-3 py-1 rounded-full ${
+                    isActive
+                      ? "text-white font-semibold bg-[#008236] border border-[#008236]"
+                      : "text-gray-700 hover:text-[#008236]"
+                  }`
+                }
+              >
+                {link.name}
+              </NavLink>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
